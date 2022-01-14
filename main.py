a5e3af4da14d4e3ee1db80c43114d775d37a34b0
adil shiekh <89821793+Shiekhboy@users.noreply.github.com>
2022-01-14 17:16:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ from core.decorators import register, language, handle_error, only_admins
 
 REPO = """
  **Music Player**
-- Repo: [GitHub](https://github.com/LEGEND-OS/MUSIC-VC)
-- Owner: @The_LegendBoy
+- Repo: [GitHub](https://github.com/Shiekhboy/MUSIC-VC)
+- Owner: @aaadilll
 """
 
 
